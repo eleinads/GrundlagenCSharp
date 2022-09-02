79938462c18f2f26d63e895667cf747da344703b
Daniele Salvatore Ferrigno <danielesal.f@gso.schule.koeln>
2022-09-02 21:06:55 +0200

    aufgaben bearbeitet

--- 1EingabeAusgabe/Program.cs
+++ 1EingabeAusgabe/Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("._.");
+﻿
+Console.WriteLine("hallo Daniele");
 Console.WriteLine("._.");
 
+string vorname = "daniele";
+string nachname = "ferrigno";
+string alter = "16";
+string klasse = "FOI21";
+
+Console.WriteLine("",vorname,"e ist ",alter," jahre alt und besucht die ",klasse," des Georg-Simon_Ohm");
