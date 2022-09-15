39c856fd3ff3e274cf095151f3df55c6abd54850
Daniele Salvatore Ferrigno <danielesal.f@gso.schule.koeln>
2022-09-15 12:56:51 +0200

    aufgabe 1 und 2 fertig bearbeitet

--- 2DatentypenKonvertierung/Program.cs
+++ 2DatentypenKonvertierung/Program.cs
@@ -1,2 +1,44 @@
 ﻿
+using System.Drawing;
+
+Console.WriteLine($"size of short: {short.MaxValue}");
+Console.WriteLine($"size of short: {short.MinValue}");
+Console.WriteLine(sizeof(short));
+
+Console.WriteLine($"size of int: {int.MaxValue}");
+Console.WriteLine($"size of int: {int.MinValue}");
+Console.WriteLine(sizeof(int));
+
+Console.WriteLine($"size of long: {long.MaxValue}");
+Console.WriteLine($"size of long: {long.MinValue}");
+Console.WriteLine(sizeof(long));
+
+Console.WriteLine($"size of float: {float.MaxValue}");
+Console.WriteLine($"size of float: {float.MinValue}");
+Console.WriteLine(sizeof(float));
+
+Console.WriteLine($"size of double: {double.MaxValue}");
+Console.WriteLine($"size of double: {double.MinValue}");
+Console.WriteLine(sizeof(double));
+
+Console.WriteLine($"size of decimal: {decimal.MaxValue}");
+Console.WriteLine($"size of decimal: {decimal.MinValue}");
+Console.WriteLine(sizeof(decimal));
+
+
+
+string vorname = "Daniele";
+short alter = 16;
+bool frage = true;
+
+
+
+
+int dollarzahl = 36;
+char dollar = (char)dollarzahl;
+
+
+
+
+
 
